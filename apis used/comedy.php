d9838f935ec8c8eb7537f939eb837d8bd56e5f50
anthonyykh <anthonykh01.ak@gmail.com>
2022-05-05 22:49:34 +0300

    added all get apis i have been using from htdocs

---
+++ apis used/comedy.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+include("db_info.php");
+
+$query = $mysqli->prepare("SELECT * FROM movies WHERE genre='comedy';");
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($movie = $array->fetch_assoc()){
+    $response[] = $movie;
+}
+
+$json_response = json_encode($response);
+echo $json_response;
+
+?>
